7d21e4b1718c0fff2df6c96a0e3eb0f485c16572
akmaurya <akmaurya@yapstone.com>
2020-02-05 17:23:32 +0530

    Add Hystrix functionality

--- src/main/java/com/aditya/ems/EmsApplication.java
+++ src/main/java/com/aditya/ems/EmsApplication.java
@@ -2,8 +2,10 @@ package com.aditya.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class EmsApplication {
 
 	public static void main(String[] args) {
